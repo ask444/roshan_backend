4a0f9c0d2e69683ded1f1bc01cc5638effd88fab
ashok <ashok.ng786@gmail.com>
2019-02-02 15:32:13 +0530

    access control header and procfile changes.

--- app/Http/Controllers/usersregistraionmsg.php
+++ app/Http/Controllers/usersregistraionmsg.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 use DB;
+use Mail;
 
+use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class usersregistraionmsg extends Controller
@@ -11,10 +13,26 @@ class usersregistraionmsg extends Controller
     public function registeruser(Request $request){
         $email = $request['email'];
         $name=$request['name'];
+        $phone=$request['phone'];
+        $gender=$request['gender'];
         $newUser=  DB::table('users')->insert(
-            ['email' => $email,'name'=>$name,'password'=>'password']
+            ['email' => $email,'name'=>$name,'password'=>'password','phone'=>$phone,'gender'=>$gender]
         );  
         if($newUser){
+
+
+            // public function basic_email() {
+                $data = array('name'=>"Virat Gandhi");
+             
+                Mail::send(['text'=>'mail'], $data, function($message) {
+                   $message->to('ashok.ng786@gmail.com', 'Tutorials Point')->subject
+                      ('Laravel Basic Testing Mail');
+                   $message->from('ashok.ng786@gmail.com','Virat Gandhi');
+                });
+                echo "Basic Email Sent. Check your inbox.";
+            //  }
+          
+
             return response()->json(array('success' => true, 'user_created' => 1), 200);      
         }
     }
