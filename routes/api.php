1876818117f785d861da1ce3dd8b6d1f11b18c32
ashok <ashok.ng786@gmail.com>
2019-02-02 17:13:29 +0530

    removed the route from route group

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
 Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
-    Route::post('usersregistration','usersregistraionmsg@registeruser');
+
 });
+
+Route::post('usersregistration','usersregistraionmsg@registeruser');
