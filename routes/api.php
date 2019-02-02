4b9648005985dc1d566ebfbb0be6b6f525e393d9
ashok <ashok.ng786@gmail.com>
2019-02-02 17:06:58 +0530

    rout added inside the route group

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
 Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
-
+    Route::post('usersregistration','usersregistraionmsg@registeruser');
 });
-
-Route::post('usersregistration','usersregistraionmsg@registeruser');
